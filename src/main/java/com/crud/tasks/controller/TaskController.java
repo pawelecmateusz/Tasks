26f2d0b8f9586007881675ecc6c6c230754d1338
Mateusz <122115887+pawelecmateusz@users.noreply.github.com>
2023-05-11 22:30:55 +0200

    22.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,13 +5,12 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/tasks")
 public class TaskController {
